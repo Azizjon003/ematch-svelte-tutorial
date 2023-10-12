3f9efd0e01bb526d7a41652e686af3d26d131839
Azizjon <aliqulovazizjon68@gmail.com>
2023-10-12 23:42:34 +0500

    updated

---
+++ src/routes/Game.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+  import Grid from "./Grid.svelte";
+  import {levels } from "./levels";
+  import type{ Level} from "./levels"
+  const level = levels[0]
+  let size:number = level.size;
+  let grid:string[] =create_grid(level)
+  let found:string[] =[]
+
+  function create_grid (level:Level){
+   
+    const copy  = level.emojis.slice()
+    console.log(copy)
+    const pairs:string[] = []  
+
+    for(let i =0;i<size**2/2;i++){
+      const index = Math.floor(Math.random() * copy.length)
+      const emoji = copy[index]
+      copy.splice(index,1)
+      pairs.push(emoji) 
+    }
+
+    pairs.push(...pairs)
+    return pairs
+  }
+</script>
+<div class="game">
+
+  <div class="info" />
+  <div class="grid-container" >
+  <Grid  {grid} />
+  </div>
+
+  <div class="info" />
+</div>
+
+<style>
+  .game {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    font-size: min(10vmin, 0.3em);
+  }
+  .info{
+    width: 80vmin;
+    height: 10vmin;
+    background-color: purple;
+  }
+  .grid-container {
+    width: 80vmin;
+    height: 80vmin;
+    background-color: teal;
+  }
+</style>
---
+++ src/routes/Grid.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  import Squere from "./Squere.svelte";
+  export let grid: string[];
+
+  let a: number = -1;
+  let b: number = -1;
+  let reset_timeout: number;
+</script>
+
+<div class="grid">
+  {#each grid as emoji, i}
+    <Squere
+      {emoji}
+      on:click={() => {
+        clearTimeout(reset_timeout);
+        if (a === -1 && b === -1) {
+          a = i;
+        } else if (b === -1) {
+          b = i;
+          if (grid[a] === grid[b]) {
+            a = -1;
+            b = -1;
+          } else {
+            reset_timeout = setTimeout(() => {
+              a = -1;
+              b = -1;
+            }, 1000);
+          }
+        } else {
+          b = -1;
+          a = i;
+        }
+      }}
+      selected={a === i || b === i}
+    />
+  {/each}
+</div>
+
+<style>
+  .grid {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: repeat(4, 1fr);
+    grid-gap: 0.5em;
+    height: 100%;
+  }
+</style>
